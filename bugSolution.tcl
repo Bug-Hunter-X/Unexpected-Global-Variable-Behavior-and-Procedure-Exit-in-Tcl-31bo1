1a505f76ad6ac155b97a3895c9d389410b646231
BugHunter-X <askzenai@gmail.com>
2024-12-27 21:02:07 +0100

    Add bugSolution.tcl file

---
+++ bugSolution.tcl
@@ -0,0 +1,23 @@
+proc error_handling {message} {
+    puts "Error: $message"
+    return -code error
+}
+
+set x 10
+set y 0
+
+if {$y == 0} {
+    error_handling "Division by zero"
+} else {
+    puts [expr {$x / $y}]
+}
+
+proc myproc {} {
+    global x
+    set x 5
+    puts "x inside proc: $x"
+    return
+}
+
+myproc
+puts "x outside proc: $x"
